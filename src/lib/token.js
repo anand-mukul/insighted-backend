0a041512aaf5be009c8e2de8531faa401d656687
Mukul Anand <mukulanand.biz@gmail.com>
2025-04-03 12:39:17 +0530

    feat: update token generation to assign refresh token to User model

--- src/lib/token.js
+++ src/lib/token.js
@@ -8,8 +8,8 @@ const generateAccessAndRefreshTokens = async (userId) => {
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
     
-    admin.refreshToken = refreshToken;
-    await admin.save({ validateBeforeSave: false });
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
 
     return { accessToken, refreshToken };
   } catch (error) {
